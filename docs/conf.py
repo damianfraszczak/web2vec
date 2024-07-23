d32a1c6f8992e87b122a5f621befa01074e88507
Edyta Frąszczak <efraszczak@motoinsight.com>
2024-07-23 10:46:53 +0200

    documentation base files

---
+++ docs/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import datetime
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../src/"))
+
+from netcenlib.version import __version__  # noqa
+
+# -- Project information -----------------------------------------------------
+now = datetime.datetime.now()
+
+
+project = "NetCenLib"
+author = "Damian Frąszczak, Edyta Frąszczak"
+copyright = f"{now.year}, {author}"
+release = __version__
+html_logo = "_static/logo.png"
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx_rtd_theme",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
---
+++ docs/index.rst
@@ -0,0 +1,32 @@
+.. NetCenLib documentation master file, created by
+   sphinx-quickstart on Tue Feb 20 15:27:13 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Web2Vec documentation!
+=====================================
+
+``Web2Vec`` (Network centrality library) is a tool to compute a wide range of centrality measures for a given network. The
+library is designed to work with Python Networkx library.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   Introduction <files/INTRODUCTION.md>
+   Quick start <files/QUICK_START.md>
+   Centrality measures <files/CENTRALITY_MEASURES.md>
+   Contributing <files/CONTRIBUTING.md>
+   Code of conduct <files/CODE_OF_CONDUCT.md>
+   Reference <source/modules>
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+.. _`Source`: https://github.com/damianfraszczak/netcenlib
+.. _`Distribution`: https://pypi.org/project/netcenlib/
